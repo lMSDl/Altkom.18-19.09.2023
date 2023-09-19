c4ae58e5def5466f0021528eb03c656343b7b85d
Paweł Łęczycki <pawel@msd.info.pl>
2023-09-19 10:51:16 +0200

    Budowa aplikacji

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -8,6 +8,70 @@ var app = builder.Build();
 //konfiguracja aplikacji
 
 
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Begin of Use1");
+
+
+    await next(context);
+
+
+    Console.WriteLine("End of Use1");
+});
+
+app.Map("/hello", helloApp =>
+{
+    helloApp.Use(async (context, next) =>
+    {
+        Console.WriteLine("Begin of HelloUse");
+
+
+        await next(context);
+
+
+        Console.WriteLine("End of HelloUse");
+    });
+
+    helloApp.Run(async context =>
+    {
+        Console.WriteLine("Begin of HelloRun");
+        await context.Response.WriteAsync("Hello o/");
+        Console.WriteLine("End of HelloRun");
+    });
+
+});
+
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Begin of Use2");
+
+
+    await next(context);
+
+
+    Console.WriteLine("End of Use2");
+});
+
+app.MapWhen(context => context.Request.Query.TryGetValue("name", out _), nameApp =>
+{
+    nameApp.Run(async context =>
+    {
+        await context.Response.WriteAsync($"Witaj {context.Request.Query["name"]}");
+    });
+
+});
+
+app.Run(async context =>
+{
+    Console.WriteLine("Begin of Run");
+    await context.Response.WriteAsync("Under Construction");
+    Console.WriteLine("End of Run");
+});
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.MapGet("/", () => "Hello from development!");
